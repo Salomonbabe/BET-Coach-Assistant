370821d608e3c8d66616e3da722d820403fed97e
Salomon <pierrebabe89@gmail.com>
2026-04-01 17:51:06 +0000

    add right url

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  SUPA_URL: "TON_URL_SUPABASE",
-  SUPA_ANON: "TON_ANON_KEY",
+  SUPA_URL: "https://tsxucwfqnbbvgtiiasyz.supabase.co",
+  SUPA_ANON: "sb_publishable_sEeuxIdHSfRH1BekmBA7ew_JbUASj2W",
 };
